97e58ede728befbb516e05f7f797aae55e86553c
Ferney Moreno <ferney.moreno@pluto.tv>
2024-03-14 18:20:25 -0500

    update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
 	video: false,
-	supportFile: false,
 	e2e: {
+		supportFile: false,
 		setupNodeEvents() {
 			// implement node event listeners here
 		},

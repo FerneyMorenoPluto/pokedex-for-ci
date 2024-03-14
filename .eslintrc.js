8101ec74dd0e5e6210b6fce515482d83a0f93121
Ferney Moreno <ferney.moreno@pluto.tv>
2024-03-14 16:29:42 -0500

    update lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,6 @@ module.exports = {
 	},
 	plugins: ['react', 'jest'],
 	rules: {
-		indent: ['error', 2],
-		'linebreak-style': ['error', 'unix'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'always'],
 		eqeqeq: 'error',
